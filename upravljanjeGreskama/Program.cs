11ef5e6cc862e2423f59aa20545277994fc523d4
Lovro Šverko <lovro.sverko@gmail.com>
2023-10-18 08:25:46 +0200

    Add files via upload

---
+++ upravljanjeGreskama/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljanjeGreskama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool kontrola=false;
+            
+            do
+            {
+                Console.Write("Upiši svoju godinu rođenja: ");
+                try
+                {
+                    godRod = Convert.ToInt16(Console.ReadLine());
+                    Console.Write("Rođen si " + godRod + " godine.");
+                    kontrola = true;
+                }
+                catch (Exception greska)
+                {
+                    Console.WriteLine(greska.Message);
+                    kontrola = false;
+                }
+            } while (kontrola == false);
+
+            Console.ReadKey();
+
+        }
+    }
+}
